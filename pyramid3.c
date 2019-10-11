b886a4dad432300e4737c848729927fc81f6d698
Shekhar <rkshekhar015@gmail.com>
2019-10-11 11:17:30 +0530

    For n number of pyramid

--- pyramid3.c
+++ pyramid3.c
@@ -2,14 +2,16 @@
 #include<conio.h>
 int main()
 {
-	int i,j,k,l;
-	for(i=1;i<=9;i++)
+	int i,j,k,l,n;
+	printf("Enter a number(odd number):");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
 	{
-		if(i<=5)
-		for(j=1;j<=6-i;j++)
+		if(i<=n/2+1)
+		for(j=1;j<=n/2+2-i;j++)
 		printf("*");
 		else
-		for(k=1;k<=i-4;k++)
+		for(k=1;k<=i-n/2;k++)
 		printf("*");
 		printf("\n");
 	}
